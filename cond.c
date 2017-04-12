6356d9597e04feb50d9976704d741fe42034b3b2
Josh Lederman <joshua@lederman.com>
2017-04-12 16:50:39 -0400

    New test case

--- cond.c
+++ cond.c
@@ -3,15 +3,20 @@
 #include <fsl_device_registers.h>
 #include "shared_structs.h"
 #include "cond.h"
+//#include "lock.c"
 
 void c_init(lock_t* l, cond_t* c) {
-	
+	c->waiting=0;
 }
 
 void c_wait(lock_t* l, cond_t* c) {
+	PIT->CHANNEL[0].TCTRL = 0x1;
+	
 	c->waiting=1;
 	current_process->cond_pointer = c;
 	process_blocked();
+	
+	PIT->CHANNEL[0].TCTRL = 0x3;
 }
 
 
@@ -20,6 +25,9 @@ int c_waiting(lock_t* l, cond_t* c) {
 }
 
 void c_signal(lock_t* l, cond_t* c) {
+	
+	PIT->CHANNEL[0].TCTRL = 0x1;
+	
 	//Loops through all processes and removes waiting on this condition
 	//The current process cannot be waiting on the same condition it signals
 	struct process_state *tmp;
@@ -28,4 +36,8 @@ void c_signal(lock_t* l, cond_t* c) {
 		tmp=tmp->nextProcess;
 		if (tmp->cond_pointer == c)
 			tmp->cond_pointer = NULL;
+	//Releases the lock
+	l_unlock(l);
+		
+	PIT->CHANNEL[0].TCTRL = 0x3;
 }
---
+++ test_case_cond_1.c
@@ -0,0 +1,45 @@
+#include "3140_concur.h"
+#include "utils.h"
+#include "lock.h"
+#include "cond.h"
+
+lock_t l;
+cond_t c;
+
+unsigned int nr= 0;
+unsigned int nw= 0;
+
+void p1 (){	
+	LEDBlue_On();
+	c_wait(&l, &c);
+	LEDBlue_Toggle();
+}
+
+void p2 () {
+	delay();
+	for (int i = 0; i < 10; i++) {
+		LEDRed_Toggle();
+		delay();
+	}
+	c_signal(&l, &c);
+}
+	
+int main (void){
+    LED_Initialize();
+
+	l_init (&l);
+	c_init (&l,&c);
+ 
+	if (process_create (p1,20) < 0) {
+	 	return -1;
+	}
+	if (process_create (p2,20) < 0) {
+	 	return -1;
+	}
+
+	process_start();
+
+  LEDGreen_On();
+	while (1) ;
+
+}
